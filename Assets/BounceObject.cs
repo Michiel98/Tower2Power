1bd1b9feabe986d99e76252bc16307f1fc8fa3ee
Michiel98 <61827915+Michiel98@users.noreply.github.com>
2022-09-25 20:50:46 +0200

    Add files via upload

---
+++ Assets/BounceObject.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class BounceObject : MonoBehaviour
+{
+    public bool Move = true;    ///gives you control in inspector to trigger it or not
+    public Vector3 MoveVector = Vector3.up; //unity already supplies us with a readonly vector representing up and we are just chaching that into MoveVector
+    public float MoveRange = 2.0f; //change this to increase/decrease the distance between the highest and lowest points of the bounce
+    public float MoveSpeed = 0.5f; //change this to make it faster or slower
+ 
+    private BounceObject bounceObject; //for caching this transform
+    private float variation;
+    private float speedVar;
+     
+    Vector3 startPosition; //used to cache the start position of the transform
+    void Start()
+    { 
+        bounceObject = this;
+        startPosition = bounceObject.transform.position;
+        variation = Random.Range(0f, 180f);
+        speedVar = Random.Range(1f, 5f);
+
+
+    }
+    void Update()
+    {
+        if(Move) //bool is checked
+                    //See if you can work out whats going on here, for your own enjoyment
+            bounceObject.transform.position = startPosition - new Vector3(0, 0.5f, 0) + MoveVector * (MoveRange * Mathf.Sin(Time.timeSinceLevelLoad * MoveSpeed * speedVar+ variation));
+ 
+    }
+}
---
+++ Assets/ParticleFollowPath.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleFollowPath : MonoBehaviour
+{
+    public string pathName;
+    public float time;
+    // Start is called before the first frame update
+
+    public void StartAnimation(Vector3 position)
+    {
+        iTween.MoveTo(gameObject, iTween.Hash("position", new Vector3(position.x, position.y+1, position.z), "easetype", iTween.EaseType.easeInOutSine, "time", time, "oncomplete", "DestroyOnComplete","oncompletetarget", gameObject));
+    }
+
+    public void DestroyOnComplete()
+    {
+        Destroy(gameObject);
+        Debug.Log("Destroyed " + gameObject);
+    }
+}
